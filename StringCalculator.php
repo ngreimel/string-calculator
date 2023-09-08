ae9424cc1e64cb39cbdb47aec26fe66ad3ad19ac
Jordan Cooper <jordan@cooperplanet.com>
2023-09-08 14:27:32 -0600

    Refactor

--- StringCalculator.php
+++ StringCalculator.php
@@ -6,12 +6,23 @@ class StringCalculator
 {
   public static function add(string $string): string
   {
+    $invalid = self::validateDelimiters($string);
+
+    if (!empty($invalid)) {
+      return $invalid;
+    }
+
+    $values = explode(',', str_replace([' ', "\n"], ['', ','], $string));
+    return (string) (array_sum($values));
+  }
+
+  protected static function validateDelimiters(string $string)
+  {
     if (preg_match('/[,\n]{2}/', $string, $matches, PREG_OFFSET_CAPTURE)) {
       $offendingCharacter = str_replace("\n", '\n', substr($matches[0][0], 1));
       $position = $matches[0][1] + 1;
       return "Number expected but '{$offendingCharacter}' found at position {$position}.";
     }
-    $values = explode(',', str_replace([' ', "\n"], ['', ','], $string));
-    return (string) (array_sum($values));
+    return null;
   }
 }
