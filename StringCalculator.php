e6f9ce968f245997da1c373220fb1792a2552aef
Jordan Cooper <jordan@cooperplanet.com>
2023-09-08 14:15:50 -0600

    Handle second delimiter

--- StringCalculator.php
+++ StringCalculator.php
@@ -6,8 +6,12 @@ class StringCalculator
 {
   public static function add(string $string): string
   {
-    $values = explode(',',str_replace(' ','',$string));
+    if (preg_match('/[,\n]{2}/', $string, $matches, PREG_OFFSET_CAPTURE)) {
+      $offendingCharacter = str_replace("\n", '\n', substr($matches[0][0], 1));
+      $position = $matches[0][1] + 1;
+      return "Number expected but '{$offendingCharacter}' found at position {$position}.";
+    }
+    $values = explode(',', str_replace([' ', "\n"], ['', ','], $string));
     return (string) (array_sum($values));
   }
 }
-
--- StringCalculatorTest.php
+++ StringCalculatorTest.php
@@ -8,49 +8,78 @@ use PHPUnit\Framework\TestCase;
 
 class StringCalculatorTest extends TestCase
 {
-    public function testEmptyStringReturnsZero(): void
-    {
-        $emptyString = '';
-        $this->assertSame("0", StringCalculator::add($emptyString));
+  public function testEmptyStringReturnsZero(): void
+  {
+    $emptyString = '';
+    $this->assertSame("0", StringCalculator::add($emptyString));
+  }
+
+  public function testAcceptsOneNumber(): void
+  {
+    $oneNumber = '1';
+    $this->assertSame($oneNumber, StringCalculator::add($oneNumber));
+  }
+
+  public function testAcceptsTwoNumbers(): void
+  {
+    $twoNumbers = "1,3";
+    $this->assertSame("4", StringCalculator::add($twoNumbers));
+  }
+
+  public function testAcceptsTwoFloats(): void
+  {
+    $twoFloats = "1.1,2.2";
+    $this->assertSame("3.3", StringCalculator::add($twoFloats));
+  }
+
+  public function testAcceptsTwoFloatsAndSpaces(): void
+  {
+    $twoFloats = "1.1        ,2.2";
+    $this->assertSame("3.3", StringCalculator::add($twoFloats));
+  }
+
+  public function testAcceptsUnknownQuantityOfNumbers(): void
+  {
+    $manyNumbers = [];
+    $limit = rand(0, 20);
+
+    for ($i = 0; $i < $limit; $i++) {
+      $manyNumbers[] = rand(0, 20);
     }
 
-    public function testAcceptsOneNumber(): void
-    {
-        $oneNumber = '1';
-        $this->assertSame($oneNumber, StringCalculator::add($oneNumber));
-    }
+    $this->assertSame(
+      (string) array_sum($manyNumbers),
+      StringCalculator::add(implode(',', $manyNumbers))
+    );
+  }
 
-    public function testAcceptsTwoNumbers(): void
-    {
-      $twoNumbers = "1,3";
-      $this->assertSame("4", StringCalculator::add($twoNumbers));
-    }
+  public function testCanHandleNewLineAsDelimiter(): void
+  {
+    $params = "1\n2,3";
 
-    public function testAcceptsTwoFloats(): void
-    {
-      $twoFloats = "1.1,2.2";
-      $this->assertSame("3.3", StringCalculator::add($twoFloats));
-    }
+    $this->assertSame(
+      '6',
+      StringCalculator::add($params)
+    );
+  }
 
-    public function testAcceptsTwoFloatsAndSpaces(): void
-    {
-      $twoFloats = "1.1        ,2.2";
-      $this->assertSame("3.3", StringCalculator::add($twoFloats));
-    }
+  public function testThrowsErrorForDuplicateDelimiter(): void
+  {
+    $params = "175.2,\n35";
 
-    public function testAcceptsUnknownQuantityOfNumbers(): void
-    {
-        $manyNumbers = [];
-        $limit = rand(0,20);
+    $this->assertSame(
+      'Number expected but \'\n\' found at position 6.',
+      StringCalculator::add($params)
+    );
+  }
 
-        for ($i=0; $i < $limit; $i++) {
-            $manyNumbers[]= rand(0,20);
-        }
+  public function testThrowsErrorForDuplicateDelimiterNewlineFirst(): void
+  {
+    $params = "175.2\n,35";
 
-        $this->assertSame(
-            (string) array_sum($manyNumbers), 
-            StringCalculator::add(implode(',',$manyNumbers))
-        );
-    }
-
+    $this->assertSame(
+      'Number expected but \',\' found at position 6.',
+      StringCalculator::add($params)
+    );
+  }
 }
