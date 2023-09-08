e7668b83e281bd27b1d8a72e015d43f3036623ca
Jarom Rial <jrial@samcart.com>
2023-09-08 13:26:42 -0600

    add new tests

--- StringCalculator.php
+++ StringCalculator.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 class StringCalculator
 {
-
+  public static function add(string $string): string
+  {
+    return (string) (empty($string) ? 0 : array_sum(explode(',', $string)));
+  }
 }
 
--- StringCalculatorTest.php
+++ StringCalculatorTest.php
@@ -10,6 +10,25 @@ class StringCalculatorTest extends TestCase
 {
     public function testEmptyStringReturnsZero(): void
     {
-        $this->assertSame(0, StringCalculator::add());
+        $emptyString = '';
+        $this->assertSame("0", StringCalculator::add($emptyString));
+    }
+
+    public function testAcceptsOneNumber(): void
+    {
+        $oneNumber = '1';
+        $this->assertSame($oneNumber, StringCalculator::add($oneNumber));
+    }
+
+    public function testAcceptsTwoNumbers(): void
+    {
+      $twoNumbers = "1,3";
+      $this->assertSame("4", StringCalculator::add($twoNumbers));
+    }
+
+    public function testAcceptsTwoFloats(): void
+    {
+      $twoFloats = "1.1,2.2";
+      $this->assertSame("3.3", StringCalculator::add($twoFloats));
     }
 }
